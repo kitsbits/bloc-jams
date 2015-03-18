751b228f1fa0e8f860bac43458cd1199fcc352c4
Kit <noblepapergraphics@gmail.com>
2015-03-18 18:41:33 -0400

    fixed sass bug

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -91,15 +91,37 @@
   globals.require.brunch = true;
 })();
 require.register("scripts/app", function(exports, require, module) {
-$(document).ready(function() {
-  $('.hero-content h3').click(function() {
-    var subText = $(this).text();
-    $(this).text(subText + "!");
-  });
+require("./landing");
+
 });
 
+;require.register("scripts/landing", function(exports, require, module) {
+$(document).ready(function() {
+  var onHoverColorAction = function(event) {
+    console.log('Hover action triggered.');
+    $(this).css({'color': '#FA9D61'});
+  };
 
+  var offHoverColorAction = function(event) {
+    console.log('Off-hover action triggered.');
+    $(this).css({'color': 'white'});
+  };
 
+  $('.hero-content h3').hover(onHoverColorAction, offHoverColorAction);
+
+  var onHoverAction = function(event) {
+    console.log('Hover action triggered.');
+    $(this).animate({'margin-top': '10px'});
+  };
+
+  var offHoverAction = function(event) {
+    console.log('Off-hover action triggered.');
+    $(this).animate({'margin-top': '0px'});
+  };
+
+  $('.selling-points .point').hover(onHoverAction, offHoverAction);
+
+});
 });
 
 ;require.register("scripts/require", function(exports, require, module) {
